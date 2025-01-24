ea16539acc1930aae933a5dc4fb5a04f52cb7389
Sumit Thakur <sumit@thexyzstudio.com>
2025-01-24 10:30:27 +0530

    feat: improve HomeTicker component; add client-side check and streamline rendering logic for better performance

--- src/appComponents/home/HomeTicker.tsx
+++ src/appComponents/home/HomeTicker.tsx
@@ -1,19 +1,24 @@
-'use client';
-import React from 'react';
-import { HorizontalTicker } from 'react-infinite-ticker';
-import HomeTickerCard, { IHomeTickerCardProps } from './HomeTickerCard';
-import { icons } from '../../../public/exporter';
+"use client";
+import React from "react";
+import { HorizontalTicker } from "react-infinite-ticker";
+import HomeTickerCard, { IHomeTickerCardProps } from "./HomeTickerCard";
+import { icons } from "../../../public/exporter";
 
 const HomeTicker = () => {
-  const RenderItem = () => {
-    return (
-      <div className="flex my-10 md:my-14 xl:my-[110px] flex-row">
-        {data.map((item, index) => (
-          <HomeTickerCard key={index} {...item} />
-        ))}
-      </div>
-    );
-  };
+  const isClient = typeof window !== "undefined";
+
+  const RenderItem = () => (
+    <div className="flex my-10 md:my-14 xl:my-[110px] flex-row">
+      {data.map((item, index) => (
+        <HomeTickerCard key={index} {...item} />
+      ))}
+    </div>
+  );
+
+  if (!isClient) {
+    return null;
+  }
+
   return (
     <div className="w-full">
       <HorizontalTicker duration={25000}>
@@ -30,27 +35,27 @@ interface IHomeTickerCardData extends IHomeTickerCardProps {
 }
 const data: IHomeTickerCardData[] = [
   {
-    name: 'Happy clients',
+    name: "Happy clients",
     icon: icons.PERSON_ICON,
-    count: '99',
+    count: "99",
     id: 1,
   },
   {
-    name: 'Events served',
+    name: "Events served",
     icon: icons.CLEANER_ICON,
-    count: '2200',
+    count: "2200",
     id: 2,
   },
   {
-    name: 'Years of experience',
+    name: "Years of experience",
     icon: icons.EXPERIENCE_ICON,
-    count: '6',
+    count: "6",
     id: 3,
   },
   {
-    name: 'Licensed officer',
+    name: "Licensed officer",
     icon: icons.LICENSE_ICON,
-    count: '2000',
+    count: "2000",
     id: 4,
   },
 ];
