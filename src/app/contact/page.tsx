f1f4012421c6fdd8ad79a8ffc182df98e5d21fa6
Sumit Thakur <sumit@thexyzstudio.com>
2025-01-28 13:19:50 +0530

    feat: add overflow-hidden to Contact component for improved layout handling

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="max-w-screen-2xl mx-auto flex flex-col pb-10 md:pb-14 xl:pb-32 gap-y-10 md:gap-y-14 xl:gap-y-32  px-6 xl:px-20">
+    <div className="max-w-screen-2xl mx-auto flex flex-col pb-10 md:pb-14 xl:pb-32 gap-y-10 md:gap-y-14 xl:gap-y-32  px-6 xl:px-20 overflow-hidden">
       <ContactFormSection />
       <MapViewSection/>
     </div>
